a4362f7dad10a2b658aa71fa01a79bf924e778be
Asad <asadrifat450@gmail.com>
2026-02-24 00:51:21 +0600

    add html file and login btn click change dynamically hide login area

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// login
+const loginBtn = document.getElementById("login");
+
+loginBtn.addEventListener("click", function(){
+    const loginArea = document.getElementById("login-area");
+    loginArea.style.display ="none";
+});
