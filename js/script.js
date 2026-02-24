2805437fb2c9a2515931cc96691c4c6d0a35ef89
Asad <asadrifat450@gmail.com>
2026-02-24 23:17:26 +0600

    login click remove login section show transection area and add deposit input

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
-// login
+// login button event handler
 const loginBtn = document.getElementById("login");
-
 loginBtn.addEventListener("click", function(){
     const loginArea = document.getElementById("login-area");
-    loginArea.style.display ="none";
+    loginArea.style.display = "none";
+
+    const transactionArea = document.getElementById("transaction-area");
+    transactionArea.style.display = "block";
+});
+
+// deposit button event handler
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function(){
+    const depositValue = document.getElementById("depositAmount").value ;
+    const depositAmount = parseFloat(depositValue);
+    
+    const currentDepositValue = document.getElementById("currentDepositAmount").innerText;
+    const currentDepositAmount = parseFloat(currentDepositValue);
+    const totalDepositAmount = depositAmount + currentDepositAmount;
+
+    document.getElementById("currentDepositAmount").innerText = totalDepositAmount;
 });
